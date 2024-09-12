3adb2d63a079cd7f783a8252e4e1ccec5e59a9c7
rodrigo-dev1 <cruz.rodrigo.rcs@gmail.com>
2024-09-11 22:09:06 -0300

    versão inicial

--- SQL/create_tables.sql
+++ SQL/create_tables.sql
@@ -9,7 +9,7 @@ CREATE TABLE CUSTOMER(
     nome          VARCHAR(100),
     sobrenome     VARCHAR(100),
     sexo          VARCHAR(20),
-    endereco      VARCHAR,
+    endereco      VARCHAR(200),
     nascimento    DATE,
     telefone      VARCHAR(30)
 );
@@ -20,14 +20,14 @@ CREATE TABLE CATEGORY(
     caminho      VARCHAR(200)
 );
 
-CREATE TABLE ITEM(
+CREATE TABLE (
     id_item            INT PRIMARY KEY,
-    nome               VARCHAR,
-    descricao          VARCHAR,
+    nome               VARCHAR(30),
+    descricao          VARCHAR(100),
     preco              DECIMAL(10,2),
     status             VARCHAR(20),
-    data_cadastro      DATE,
-    data_cancelamento  DATE,
+    data_cadastro      TIMESTAMP,
+    data_cancelamento  TIMESTAMP,
     id_category        INT,
     FOREIGN KEY (id_category) REFERENCES Category(id_category)
 );
@@ -37,7 +37,7 @@ CREATE TABLE ORDERS(
     id_order     INT PRIMARY KEY,
     id_customer  INT,
     id_vendedor  INT,
-    data_pedido  DATE,
+    data_pedido  TIMESTAMP,
     total        Decimal(10,2),
     FOREIGN KEY (id_vendedor) REFERENCES Customer(id_customer),
     FOREIGN KEY (id_customer) REFERENCES Customer(id_customer)
@@ -51,3 +51,14 @@ CREATE TABLE ORDERITEM (
     FOREIGN KEY (id_order) REFERENCES Orders(id_order),
     FOREIGN KEY (id_item) REFERENCES Item(id_item)
 );
+
+
+
+CREATE TABLE STATUSITEM_DAILY (
+    id_item INT,
+    preco DECIMAL(10,2),
+    status VARCHAR(20),
+    data_atualizacao DATE
+    FOREIGN KEY (id_item) REFERENCES ITEM(id_item),
+);
+
